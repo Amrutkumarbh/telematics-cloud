b76ab3a6461d2d5c12ea130f2dfc0fc75f9b4c8f
Amrutkumarbh <amrutkumarbh@gmail.com>
2026-05-18 14:44:01 +0530

    added remote command system

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ app = Flask(__name__)
 
 latest_data = {}
 
+vehicle_command = "NONE"
+
+# HOME PAGE
 @app.route('/')
 def home():
     return "Telematics Server Running"
 
+# RECEIVE TELEMETRY FROM ESP32
 @app.route('/api/telemetry', methods=['POST'])
 def telemetry():
 
@@ -22,10 +26,41 @@ def telemetry():
         "status": "received"
     })
 
+# GET LATEST TELEMETRY
 @app.route('/api/latest')
 def latest():
 
     return jsonify(latest_data)
+
+# ESP32 READS COMMAND FROM HERE
+@app.route('/api/command')
+def command():
+
+    global vehicle_command
+
+    return jsonify({
+        "command": vehicle_command
+    })
+
+# IMMOBILIZE COMMAND
+@app.route('/api/immobilize')
+def immobilize():
+
+    global vehicle_command
+
+    vehicle_command = "IMMOBILIZE"
+
+    return "IMMOBILIZE COMMAND SENT"
+
+# MOBILIZE COMMAND
+@app.route('/api/mobilize')
+def mobilize():
+
+    global vehicle_command
+
+    vehicle_command = "MOBILIZE"
+
+    return "MOBILIZE COMMAND SENT"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
